62d7eef929a81176daafc5793c4c44266ca7dda1
Oyedolacode <segunzyshegszy12@gmail.com>
2021-03-03 15:31:21 -0800

    Update codebase

--- Registration System/db.php
+++ Registration System/db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $conn = mysqli_connect('localhost', 'hustle', 'test1234', 'loginsystem');
+    $conn = mysqli_connect('localhost', 'id16297708_1base', '+H%)IE(u@}e8{xHX' , 'id16297708_loginsystem','3306');
     // Check connection
     if (!$conn){
         echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
--- Registration System/registration.php
+++ Registration System/registration.php
@@ -13,15 +13,15 @@
         // removes backslashes
         $username = stripslashes($_REQUEST['username']);
         //escapes special characters in a string
-        $username = mysqli_real_escape_string($con, $username);
+        $username = mysqli_real_escape_string($conn, $username);
         $email    = stripslashes($_REQUEST['email']);
-        $email    = mysqli_real_escape_string($con, $email);
+        $email    = mysqli_real_escape_string($conn, $email);
         $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
+        $password = mysqli_real_escape_string($conn, $password);
         $create_datetime = date("Y-m-d H:i:s");
         $query    = "INSERT into `users` (username, password, email, create_datetime)
                      VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
-        $result   = mysqli_query($con, $query);
+        $result   = mysqli_query($conn, $query);
         if ($result) {
             echo "<div class='form'>
                   <h3>You are registered successfully.</h3><br/>
@@ -41,7 +41,7 @@
         <input type="text" class="login-input" name="email" placeholder="Email Adress">
         <input type="password" class="login-input" name="password" placeholder="Password">
         <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link">Already have an account? <a href="login.php">Login here</a></p>
+        <p class="link"><a href="login.php">Click to Login</a></p>
     </form>
 <?php
     }
